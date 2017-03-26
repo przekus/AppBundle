6b1b2b5521e64deb5088fa67e807272b0618c903
przekus <przekus@wp.pl>
2017-03-26 17:03:12 +0200

    initial commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace p3s\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('p3sAppBundle:Default:index.html.twig');
+    }
+}
---
+++ p3sAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace p3s\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class p3sAppBundle extends Bundle
+{
+}
